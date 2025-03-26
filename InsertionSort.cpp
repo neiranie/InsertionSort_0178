3a6de816270dc6acd9d7b4c0d655fa393c69b81d
neiranie <nurkhairanianneira@gmail.com>
2025-03-26 15:59:31 +0700

    Menambahkan Looping dengan j dimulai dari 0 hingga n-1

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -38,3 +38,33 @@ void insertionSort()
 int temp; // Membuat variable data temporer atau penyimpanan sementara
 int j, i; // Membuat variable j sebagai penanda
 
+for (i = 1; i <= n - 1; i++)
+{ // Step 1
+
+temp = arr[i]; // Step 2
+
+j = i - 1; // Step 3
+
+while (j >= 0 && arr [j] > temp) // Step 4
+{
+arr[j + 1] = arr [j]; // Step 4a
+j--; // Step 4b
+}
+
+arr[j + 1] = temp; // Step 5
+}
+}
+
+void display()
+{
+cout << endl; // Output baris Kosong
+cout << "==================================" << endl;
+cout << "Total Passs = " << n - 1 << endl; // count element movement
+cout << "==================================" << endl; // Output ke layar
+cout << "Element Array yang telah tersusun" << endl; // Output ke layar
+cout << "==================================" << endl; // Output ke layar
+
+for (int j = 0; j < n; j++)
+{ // Looping dengan j dimulai dari 0 hingga n-1}
+cout << arr [j] << endl; // Output ke layar
+}
